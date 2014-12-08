ab6943e128344295357d28b93773f7007fc24029
Jahsie Ault <jahsieault@gmail.com>
2014-12-07 19:35:24 -0800

    Added scroll to bottom, worked on animated welcome message

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* window.onload=toBottom; 
+function toBottom() { 
+	alert("Scrolling to bottom ..."); 
+	window.scrollTo(0, document.body.scrollHeight); 
+} 
+
+$( document.body ).onload(function() {
+  $( ".welcome" ).fadeIn( "3000" );
+});
+
+
+*/
+
+window.scrollTo(0,document.body.scrollHeight);
+
+
+/*
+
+$( "#btn1" ).click(function() {
+  $( ".welcome" ).fadeOut( 1600);
+});
+
+*/
